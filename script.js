f9a1e89739936f91a3397188e3b486caa533fabb
michaeltheboyer <michael.boyer@ocsbstudent.ca>
2021-02-24 17:01:24 +0000

    Javascript fixes.

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ document.getElementById('button').addEventListener('click', mathFunction)
 function mathFunction () {
   const num1 = parseInt(document.getElementById('input1').value)
   const num2 = parseInt(document.getElementById('input2').value)
-  let answer = document.getElementById('answer')
+  const answer = document.getElementById('answer')
   let total = 0
   if (num1 && num2) {
-    for (let i = 0; i<num1; i++) {
+    for (let i = 0; i < num1; i++) {
       total = total + num2
     }
     answer.innerHTML = 'Total is ' + total
